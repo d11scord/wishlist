d84316ac8564a10b2b403265088bbfff9a0c9982
maximborodkin <maxim.borodkin1999@yandex.ru>
2019-10-12 16:29:36 +0300

    header added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,52 @@
 import React from 'react';
 import './Header.css';
+import Avatar from '../../components/User/Avatar';
 
 class Header extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            leftSide: null,
+            rightSide: null,
+            center: null,
+        };
+    }
+
     render(){
         return(
-            <p>jopa</p>
+            <div className="Header">
+                <div className="left">
+                    <DrawMe/>
+                </div>
+                <div className="right">
+                    <DrawFriends/>
+                </div>
+            </div>
         )
     }
 }
 
+function DrawMe() {
+    return (
+        <div className="my-profile-block">
+            <Avatar/>
+            <span className="name">Юля Бородкина</span>
+        </div>
+    )
+}
+
+function DrawFriends(){
+    return (
+        <div className="friends-block">
+            <div className="friends-avatars">
+                <Avatar className={"avatar-first"}/>
+                <Avatar className="avatar-second"/>
+                <Avatar className="avatar-third"/>
+            </div>
+            <span className="name">Мои друзья</span>
+        </div>
+    )
+}
+
 export default Header;
+

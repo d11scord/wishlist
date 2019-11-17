6907c1830076886d20e4816f48bf09990d9de36b
maximborodkin <maxim.borodkin1999@yandex.ru>
2019-11-17 13:24:20 +0300

    popka

--- src/components/FriendsList/FriendsList.js
+++ src/components/FriendsList/FriendsList.js
@@ -21,6 +21,7 @@ class FriendsList extends React.Component {
             nowDisplayed: 0
         };
         //Кто сейчас это читает, тот самый лучший
+        //Кто написал это, тот самый лучший <3
     }
 
     render(){
--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import styled from "styled-components";
 import api from './../../api.js';
 import Suggestions from "./Suggestions";
@@ -25,9 +25,19 @@ const InputField = styled.input`
     outline: none;
 `;
 
+const UserInput = styled.span`
+    color: red;
+`;
+
+const Completion = styled.span`
+    color: b2b2b2;
+`;
+
 class Input extends React.Component{
     state = {
         query: '',
+        inputText: '',
+        completion: '',
         results: [],
         isEmpty: true,
     };
@@ -35,8 +45,10 @@ class Input extends React.Component{
     getInfo = () => {
         api(`/api/products/suggest?query=${this.state.query}`)
             .then(data => {
-                console.log(data);
+                console.log(data.response.suggestions.input.value);
                 this.setState({
+                    inputText: data.response.suggestions.input.value,
+                    completion: data.response.suggestions.completions[0].completion,
                     results: data.response.suggestions.completions.map((sugg) => sugg.value),
                     isEmpty: false,
                 })
@@ -48,27 +60,26 @@ class Input extends React.Component{
             query: this.search.value,
         }, () => {
             if (this.state.query && this.state.query.length > 1) {
-                if (this.state.query.length % 2 === 0) {
-                    this.getInfo()
-                }
+                this.getInfo()
             } else if (!this.state.query) {
             }
         })
     };
 
     render() {
+        const userInput = <Fragment><UserInput>kok</UserInput><Completion>jk</Completion></Fragment>;
         let temp;
         if (!this.state.isEmpty){
             temp = <Suggestions results={this.state.results} />
         }
         return (
             <Container isEmpty={this.state.isEmpty}>
+                {userInput}
                 <InputField
                     placeholder={this.props.searchPlaceholder}
                     ref={input => this.search = input}
                     onChange={this.handleInputChange}
                 />
-                {temp}
             </Container>
         )
     }
